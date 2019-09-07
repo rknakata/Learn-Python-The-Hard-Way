2c9aad3ae417a9fa9b512dcf4b772bed211fac64
ryan <rknakata@hawaii.edu>
2019-09-07 02:01:31 -1000

    sleep

--- ex3.py
+++ ex3.py
@@ -1,17 +1,27 @@
 print("I will now count my chickens:")
+print("code: print(\"I will now count my chickens:\")")
 
 print("Hens", 25 + 30 / 6)
+print("code: print(\"Hens\", 25 + 30 / 6)")
 print("Roosters", 100 - 25 * 3 % 4)
+print("code: print(\"Roosters\", 100 - 25 * 3 % 4)")
 
 print("Now I will count the eggs:")
+print("code: print(\"Now I will count the eggs:\")")
 
 print(3 + 2 + 1 -5 + 4 % 2 - 1 /4 + 6)
+print("code: print(3 + 2 + 1 -5 + 4 % 2 - 1 /4 + 6)")
 
 print("Is it true that 3 + 2 < 5 - 7?")
+print("code: print(\"Is it true that 3 + 2 < 5 - 7?\")")
 
 print(3 + 2 < 5 - 7)
+print("code: print(3 + 2 < 5 - 7)")
 
 print("What is 3 + 2", 3 + 2)
-print("What is 5 - 7?", 5 - 7)
+print("code: print(\"What is 3 + 2\", 3 + 2)")
+print("What is 5 - 7?", 5 - 7) # the equation on the right gets printed one space after the "?" doesnt matter if you delete the space before the 5
+print("code: print(\"What is 5 - 7?\", 5 - 7)")
 
 print("Oh, that's why it's False.")
+print("code: print(\"Oh, that's why it's False.\")")
